9069d21ce02e9a5591e33d4046a19cbb8c400e1f
santiagoSosa <sosamarinsantiago43@gmail.com>
2025-03-01 22:26:25 -0500

    segunda prueba

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
 class GestorDioses {
     constructor() {
         this.dioses = [
-            { nombre: "Zeus", imagen: "../images/zeus.jpg", descripcion: "Dios del trueno y el rey del Olimpo." },
-            { nombre: "Poseidon", imagen: "../images/poseidon.jpg", descripcion: "Dios del mar y los terremotos." },
-            { nombre: "Hades", imagen: "../images/hades.jpg", descripcion: "Dios del inframundo." },
-            { nombre: "Hera", imagen: "../images/hera.jpg", descripcion: "Diosa del matrimonio y la familia." },
-            { nombre: "Ares", imagen: "../images/ares.jpg", descripcion: "Dios de la guerra." },
-            { nombre: "Atenea", imagen: "../images/Atenea.webp", descripcion: "Diosa de la sabiduría y la estrategia." },
-            { nombre: "Apolo", imagen: "../images/apolo.jpg", descripcion: "Dios del sol, la música y la profecía." },
-            { nombre: "Artemisa", imagen: "../images/artemisa.jpg", descripcion: "Diosa de la caza y la naturaleza." },
-            { nombre: "Hefesto", imagen: "../images/hefesto.webp", descripcion: "Dios del fuego y la forja." },
-            { nombre: "Afrodita", imagen: "../images/afrodita.jpg", descripcion: "Diosa del amor y la belleza." },
-            { nombre: "Hermes", imagen: "../images/hermes.webp", descripcion: "Dios mensajero y del comercio." },
-            { nombre: "Deméter", imagen: "../images/demeter.jpg", descripcion: "Diosa de la agricultura y la cosecha." }
+            { nombre: "Zeus", imagen: "images/zeus.jpg", descripcion: "Dios del trueno y el rey del Olimpo." },
+            { nombre: "Poseidon", imagen: "images/poseidon.jpg", descripcion: "Dios del mar y los terremotos." },
+            { nombre: "Hades", imagen: "images/hades.jpg", descripcion: "Dios del inframundo." },
+            { nombre: "Hera", imagen: "images/hera.jpg", descripcion: "Diosa del matrimonio y la familia." },
+            { nombre: "Ares", imagen: "images/ares.jpg", descripcion: "Dios de la guerra." },
+            { nombre: "Atenea", imagen: "images/Atenea.webp", descripcion: "Diosa de la sabiduría y la estrategia." },
+            { nombre: "Apolo", imagen: "images/apolo.jpg", descripcion: "Dios del sol, la música y la profecía." },
+            { nombre: "Artemisa", imagen: "images/artemisa.jpg", descripcion: "Diosa de la caza y la naturaleza." },
+            { nombre: "Hefesto", imagen: "images/hefesto.webp", descripcion: "Dios del fuego y la forja." },
+            { nombre: "Afrodita", imagen: "images/afrodita.jpg", descripcion: "Diosa del amor y la belleza." },
+            { nombre: "Hermes", imagen: "images/hermes.webp", descripcion: "Dios mensajero y del comercio." },
+            { nombre: "Deméter", imagen: "images/demeter.jpg", descripcion: "Diosa de la agricultura y la cosecha." }
         ];
         this.tarjetasContainer = document.getElementById("tarjetasContainer");
         this.cargarTarjetas();
@@ -38,7 +38,7 @@ class GestorDioses {
         document.getElementById("modal").classList.remove("oculto");
 
         document.getElementById("masInfoBtn").onclick = function () {
-            window.location.href = `../detalles/${dios.nombre.toLowerCase()}.html`;
+            window.location.href = `detalles/${dios.nombre.toLowerCase()}.html`;
         };
     }
 }
